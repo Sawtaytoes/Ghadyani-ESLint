9e1f01d6c2db2b0a24d81bf22909f1410d446b45
Kevin Ghadyani <Saturn2888@gmail.com>
2020-02-28 00:30:40 -0600

    Added tests to core/

---
+++ packages/core/tests/fixed.js
@@ -0,0 +1,207 @@
+let blah
+
+blah = []
+
+blah = [1]
+
+blah = [1]
+
+blah = [
+	1,
+	2,
+]
+
+blah = [1]
+
+blah = [
+	1,
+	2,
+]
+
+blah = [
+	1,
+	2,
+]
+
+console.log(blah)
+
+let func
+
+func = ({
+	hello,
+	someone,
+}) => ({
+	hello,
+	someone,
+})
+
+func = ({
+	hello,
+	someone,
+}) => ({
+	hello,
+	someone,
+})
+
+func = (
+	{
+		hello,
+		someone,
+	},
+	stuff,
+) => ({
+	hello,
+	someone,
+	stuff,
+})
+
+func = ({
+	hello,
+	someone,
+}) => (
+	!someone
+	? someone
+	: hello
+)
+
+func = ({
+	hello,
+	someone,
+}) => (
+	!someone
+	? someone
+	: hello
+)
+
+func = ({
+	hello,
+	someone,
+}) => ({
+	stuff,
+}) => (
+	someone
+	? stuff
+	: hello
+)
+
+func
+.call(func)
+
+const test = (
+	func()
+	? (
+		blah
+		? 'phil'
+		: 'kevin'
+	)
+	: 'jacob'
+)
+
+console.log(test)
+
+if (test == 2) {
+	blah
+} else if (blah === 2) {
+	func()
+} else {
+	func(blah)
+}
+
+if (test == 2) {
+	blah
+}
+else if (blah === 2) {
+	func()
+}
+else {
+	func(blah)
+}
+
+const object = {
+	blah: 'blah',
+	fun: 'fun',
+}
+
+object
+
+const of = Function
+const map = Function
+const filter = Function
+
+const observable$ = (
+	of(blah)
+	.pipe(
+		map(func),
+		filter(Boolean),
+	)
+)
+
+observable$.subscribe()
+.unsubscribe()
+
+observable$.subscribe()
+
+let d3
+
+d3.select('body')
+.selectAll('p')
+.data([
+	4,
+	8,
+	15,
+	16,
+	23,
+	42,
+])
+.enter()
+.append('p')
+.text(function(d) { return 'I\'m number ' + d + '!' })
+
+let someReallyLongArrayName = []
+
+someReallyLongArrayName
+.map(({
+	bar,
+	foo,
+}) => ({
+	bar,
+	foo,
+}))
+.filter(Boolean)
+
+const newObject = {
+	[`
+		some
+		text
+	`]: true,
+	a: 1,
+	[blah]: [
+		2,
+		{
+			a: 3,
+			b: 4,
+		},
+	],
+}
+
+newObject
+
+let value
+
+value = 1 + 2 + 4
+
+value = (
+	1
+	+ 2
+	+ 4
+)
+
+value
+
+const stuff = 1
+const phil = 2
+const kevin = 3
+
+stuff
+phil
+kevin
