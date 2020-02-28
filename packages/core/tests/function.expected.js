7f5439fc8e9d75f16ad29c1ae212edebe98ab258
Kevin Ghadyani <Saturn2888@gmail.com>
2020-02-28 05:06:36 -0600

    Added function tests

---
+++ packages/core/tests/function.expected.js
@@ -0,0 +1,68 @@
+let func
+
+func = ({
+	hello,
+	someone,
+}) => (
+	!someone
+	? someone
+	: hello
+)
+
+func = ({
+	hello,
+	someone,
+}) => (
+	!someone
+	? someone
+	: hello
+)
+
+func = ({
+	hello,
+	someone,
+}) => (
+	!someone
+	? someone
+	: hello
+)
+
+func = ({
+	hello,
+	someone,
+}) => (
+	!someone
+	? someone
+	: hello
+)
+
+func = ({
+	hello,
+	someone,
+}) => (
+	!someone
+	? someone
+	: hello
+)
+
+func = ({
+	hello,
+	someone,
+}) => (
+	!someone
+	? someone
+	: hello
+)
+
+func = ({
+	hello,
+	someone,
+}) => ({
+	stuff,
+}) => (
+	someone
+	? stuff
+	: hello
+)
+
+func()
---
+++ packages/core/tests/function.original.js
@@ -0,0 +1,79 @@
+let func
+
+func = ({
+	hello,
+	someone,
+}) =>
+	!someone
+	? someone
+	: hello
+
+func = ({
+	hello,
+	someone,
+}) =>
+(
+
+
+	!someone
+	? someone
+	: hello
+
+
+)
+
+func = ({
+	hello,
+	someone,
+}) =>
+(
+	!someone
+	? someone
+	: hello
+)
+
+func = ({
+	hello,
+	someone,
+}) =>
+(
+	!someone
+	? someone
+	: hello )
+
+func = ({
+	hello,
+	someone,
+}) =>
+(!someone
+	? someone
+	: hello)
+
+func = ({
+	hello,
+	someone,
+}) => (!someone
+	? someone
+	: hello)
+
+func = ({
+	hello,
+	someone,
+}) => (
+	!someone
+	? someone
+	: hello
+)
+
+func = ({
+	hello,
+	someone,
+}) => ({
+	stuff,
+}) => (
+	someone
+	? stuff
+	: hello
+)
+
+func()
